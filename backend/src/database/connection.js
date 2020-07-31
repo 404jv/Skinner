1e5b4781b755cfba78b83fc54294ad62d1012457
paulodevsilva <silva.paulohr@gmail.com>
2020-07-31 11:46:59 -0300

    create database class

--- backend/src/database/connection.js
+++ backend/src/database/connection.js
@@ -1,19 +1,35 @@
 const ibm_db = require('ibm_db');
-let connection;
 
-const connectDatabase = async () => {
-  try {
-    connection = await ibm_db.open(process.env.DB_URL)
-    console.log('> Database : online');
-
-    return connection
-  } catch (err) {
-    console.error('> Database : offline')
-    return err
+class Database {
+  constructor() {
+    this.connection;
+    this.connect()
   }
+
+  async connect() {
+    try {
+      this.connection = await ibm_db.open(process.env.DB_URL)
+
+      console.log('> Database : online');
+
+      return this.connection
+    } catch (err) {
+      console.error('> Database : offline')
+      return err
+    }
+
+  }
+
+  async query(sql) {
+    try {
+      const data = await this.connection.query(sql)
+
+      return { status: 'ok', data }
+    } catch (err) {
+      return { status: 'err' }
+    }
+  }
+
 }
 
-connectDatabase();
-
-
-module.exports = connection;
+module.exports = new Database();
