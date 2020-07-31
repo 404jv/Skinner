7ad280e78b36a10c7caa8e0c6cbfab0b1630afec
João Victor Ramalho <joaovictorramalho7@gmail.com>
2020-07-30 22:40:39 -0300

    Profile da Aplicação

--- mobile/src/pages/Profile/index.js
+++ mobile/src/pages/Profile/index.js
@@ -1,12 +1,73 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { 
+  View, 
+  Text,
+  Image,
+  TouchableOpacity,
+  TextInput,
+  KeyboardAvoidingView
+} from 'react-native';
+import { Feather as Icon } from '@expo/vector-icons';
+
 
 import styles from './styles';
 
 export default function Home() {
   return (
-    <View style={styles.container}>
-      <Text>Hello, Home!.</Text>
-    </View>
+    <KeyboardAvoidingView 
+      style={styles.container}
+      behavior='padding'
+    >
+      <View style={styles.header}>
+        <Image 
+          style={styles.image}
+          source={{ uri: 'https://images.daznservices.com/di/library/GOAL/23/bc/neymar-psg-ligue-1_zv7n75ib8ihzjlour392nac.jpg?t=432816832&quality=100' }}        
+        />
+        
+        <TouchableOpacity
+          style={styles.buttonCam}
+          activeOpacity={0.6}
+        >
+          <Icon 
+            name="camera" 
+            size={25} 
+            color="#FFF"
+            style={{ margin: 5}}
+          />
+        </TouchableOpacity>
+        <Text style={styles.name}>Dr. Luan</Text>
+      </View>
+
+      <TouchableOpacity
+        activeOpacity={0.6}
+        style={styles.buttonEdit}
+      >
+        <Text style={styles.buttonText}>Edit Profile</Text>
+        <Icon 
+          name="edit"
+          size={20}
+          color="#0D0D0D"
+        />
+      </TouchableOpacity>
+
+      <View style={styles.inputGroup}>
+        <Text style={styles.label}>About</Text>
+        <View style={styles.input}>
+          <Text style={styles.textInput}>
+            Formado na USP mestrado em Harvard. Trabalhando na Laboratório Irineu
+          </Text>
+        </View>
+
+        <Text style={styles.label}>E-mail</Text>
+        <View style={styles.input}>
+          <Text style={styles.textInput}>menino7ney@bacelona.com</Text>
+        </View>
+
+        <Text style={styles.label}>Password</Text>
+        <View style={styles.input}>
+          <Text style={styles.textInput}>*******</Text>
+        </View>
+      </View>
+    </KeyboardAvoidingView>
   );
 }
--- mobile/src/pages/Profile/styles.js
+++ mobile/src/pages/Profile/styles.js
@@ -1,8 +1,85 @@
 import { StyleSheet } from 'react-native'
+import Constants from 'expo-constants';
 
 export default StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center'
-  }
+    backgroundColor: '#FFF',
+    paddingTop: Constants.statusBarHeight + 8,
+    alignItems: 'center'
+  },
+
+  header: {
+    alignItems: 'center'
+  },
+
+  image: {
+    width: 120,
+    height: 120,
+    borderRadius: 60,
+  },
+
+  buttonCam: {
+    width: 35,
+    height: 35,
+    backgroundColor: '#FADA00',
+    bottom: 30,
+    left: 40,
+    borderRadius: 18,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+  name: {
+    bottom: 20,
+    fontFamily: 'Ubuntu_700Bold',
+    fontSize: 20,
+    color: '#898988'
+  },
+
+  buttonEdit: {
+    width: '90%',
+    height: 30,
+    borderRadius: 8,
+    justifyContent: 'center',
+    alignItems: 'center',
+    flexDirection: 'row',
+    marginTop: 12,
+    backgroundColor: '#F5F5F5',
+
+  },
+
+  buttonText: {
+    fontFamily: 'Roboto_400Regular',
+    fontSize: 15,
+    marginRight: 8,
+    color: '#0D0D0D',
+  },
+
+  inputGroup: {
+    width: '90%'
+  },
+
+  label: {
+    fontFamily: 'Ubuntu_700Bold',
+    fontSize: 20,
+    margin: 8,
+    color: '#0D0D0D'
+  },
+
+  input:{
+    height: 55,
+    backgroundColor: '#F5F5F5',
+    borderRadius: 8,
+    justifyContent: 'center',
+    paddingLeft: 16,
+    padding: 8
+  },
+
+  textInput: {
+    textAlign: 'left',
+    fontFamily: 'Roboto_400Regular',
+    fontSize: 16
+  },
+
 });
