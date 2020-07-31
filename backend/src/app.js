66e0aed633349bb71869ed599b8696d59dcb83bc
paulodevsilva <silva.paulohr@gmail.com>
2020-07-31 11:48:58 -0300

    call database connect

--- backend/src/app.js
+++ backend/src/app.js
@@ -5,15 +5,16 @@ const bodyParser = require('body-parser');
 const morgan = require('morgan');
 const cors = require('cors');
 
-const db = require('./database/connection');
+const DB = require('./database/connection');
+
 
 const app = express();
 
 app.io = require('socket.io')();
 
 const routes = require('./routes')({ io: app.io });
-const chat = require('./utils/chat')({ io: app.io, db });
-const queries = require('./utils/queries')({ db });
+const chat = require('./utils/chat')({ io: app.io });
+// const queries = require('./utils/queries')({ db });
 
 app.use(express.json());
 app.use(bodyParser.json());
